e89b0d09b6ac5dbc03ec28b2aa23d5861dc10233
kendry <kendry@sumerlabs.com>
2023-01-25 19:57:36 -0500

    carpetas

---
+++ src/components/CardSale/CardSale.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CardSale.scss'
+
+const CardSale = () => {
+    return(
+        <div className='prueba'>
+            Card Sale
+        </div>
+    )
+};
+
+export default CardSale;
